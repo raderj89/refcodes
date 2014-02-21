33ec5c87ceedcf5d5b85cbdc5261fae3db9c167e
Jared Rader <raderj89@gmail.com>
2014-02-21 14:35:06 -0600

    added claim functionality to link, improved URL validator message

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -3,7 +3,7 @@ class Referral < ActiveRecord::Base
   default_scope -> { order('created_at DESC') }
   validates :company_id, presence: true
   validates :details, presence: true, length: { maximum: 140 }
-  validates :link, presence: true, url: true
+  
 
   belongs_to :company
   has_many :claims, dependent: :destroy
@@ -37,5 +37,4 @@ class Referral < ActiveRecord::Base
   def create_claim
     self.claims.create
   end
-
 end
